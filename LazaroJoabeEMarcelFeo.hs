b4a5a7ca14fa1205b9460ed61081a5d7fb649bae
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-11-15 15:19:17 -0300

    1 e 2 feitos

---
+++ LazaroJoabeEMarcelFeo.hs
@@ -0,0 +1,9 @@
+type Student = (Int, String, String, Int)
+
+fulano, sicrano :: Student
+
+fulano = (1, "Fulano", "Pereia", 16)
+sicrano = (2, "Sicrano", "Silva", 18)
+
+initialDataBase :: [Student]
+initialDataBase = [fulano, sicrano]
