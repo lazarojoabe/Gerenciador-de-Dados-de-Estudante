c1f52af560c0695d5a6f57a667358f6b90e3c1f3
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-11-16 17:53:13 -0300

    Menu criado, funcao de insercao e impressao do banco de dados criada

--- LazaroJoabeEMarcelFeo.hs
+++ LazaroJoabeEMarcelFeo.hs
@@ -1,9 +1,48 @@
 type Student = (Int, String, String, Int)
 
-fulano, sicrano :: Student
-
-fulano = (1, "Fulano", "Pereia", 16)
-sicrano = (2, "Sicrano", "Silva", 18)
-
 initialDataBase :: [Student]
-initialDataBase = [fulano, sicrano]
+initialDataBase = []
+
+adicionarEstudante :: [Student] -> IO [Student]
+adicionarEstudante initialDataBase = do 
+    novoEstudante <- lerDadosNovoEstudante
+    let baseDadosAtualizada = novoEstudante : initialDataBase
+    print baseDadosAtualizada
+    return baseDadosAtualizada
+
+--imprimirDB :: IO()
+--imprimirDB = print initialDataBase
+
+menu:: [Student] -> IO()
+menu database = do
+    putStrLn "SEJA BEM-VINDO(A)!!!!"
+    putStrLn "1. Adicionar Estudante"
+    putStrLn "2. Recuperar Estudante por ID"
+    putStrLn "3. Atualizar Informações do Estudante"
+    putStrLn "4. Sair"
+    putStrLn "Escolha uma opção:"
+    op <- getLine
+
+    case op of
+        "1" ->
+            do
+                novaDataBase <- adicionarEstudante database
+                menu novaDataBase
+
+        "4" -> putStrLn "Saindo..."
+
+
+lerDadosNovoEstudante:: IO Student    
+lerDadosNovoEstudante = do
+    putStrLn "Insira o ID do estudante que deseja inserir: "
+    id <- readLn
+    putStrLn "Insira o primeiro nome do Estudante: "
+    nome <- getLine
+    putStrLn "Insira o sobrenome do Estudante: "
+    sobrenome <- getLine
+    putStrLn "Insira a idade do Estudante: "
+    idade <- readLn
+    return (id, nome, sobrenome, idade)
+
+main::IO()
+main = menu initialDataBase
